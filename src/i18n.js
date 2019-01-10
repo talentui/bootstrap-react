891e4f0cf7b6359ee7bdbc2880932cf92d8ef2c9
Raymond Li <it1011@qq.com>
2019-01-10 18:00:26 +0800

    feat: 🎸 添加Beiseni18n方案

---
+++ src/i18n.js
@@ -0,0 +1,27 @@
+import I18NTools from '@beisen/i18n-tools';
+import appConfig from '_/src/app-config';
+const { i18n } = appConfig;
+
+export default function(kickoff) {
+  const { BSGlobal = {} } = window;
+  const { I18NInfo } = BSGlobal;
+  if (I18NInfo) {
+    window.I18NTools = I18NTools;
+    I18NTools.appInit({
+      lng: i18n.lng || I18NInfo.I18NUserLang,
+      i18nSite: I18NInfo.I18NFrontEndHost,
+      versions: function() {
+        var version = {};
+        version[I18NInfo.I18NUserLang] = I18NInfo.I18NVersion;
+        return version;
+      },
+      application: i18n.app,
+      platform: i18n.platform,
+      callback: function() {
+        kickoff();
+      }
+    });
+    return;
+  }
+  kickoff();
+}
--- src/index.js
+++ src/index.js
@@ -1,43 +1,46 @@
-import React from "react";
-import { render } from "react-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import composeReducer from "./compose-reducers";
+import React from 'react';
+import { render } from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import composeReducer from './compose-reducers';
 import config from '_/src/app-config';
+import initI18N from './i18n';
 
 //import from local file
-import PageProxy from "./page-proxy";
-import configureStore from "./configure-store";
+import PageProxy from './page-proxy';
+import configureStore from './configure-store';
 
-import reducer from "_/src/reducers";
+import reducer from '_/src/reducers';
 
 let store = configureStore(reducer);
 let mountTarget = config.el || 'bsMain';
 
-if(typeof(mountTarget) === 'string'){
-    mountTarget = document.getElementById(mountTarget);
+if (typeof mountTarget === 'string') {
+  mountTarget = document.getElementById(mountTarget);
 }
 
 const bootstrap = node => {
-    render(
-        <Provider store={store}>
-            <Router>
-                <Route path="*" component={PageProxy} />
-            </Router>
-        </Provider>,
-        node
-    );
+  render(
+    <Provider store={store}>
+      <Router>
+        <Route path="*" component={PageProxy} />
+      </Router>
+    </Provider>,
+    node
+  );
 };
 
-bootstrap(mountTarget);
+const start = function() {
+  bootstrap(mountTarget);
+};
+
+initI18N(start);
 
 if (module.hot) {
-    module.hot.accept("./page-proxy", () => {
-        bootstrap(mountTarget);
-    });
+  module.hot.accept('./page-proxy', start);
 
-    module.hot.accept("_/src/reducers", () => {
-        let reducer = require("_/src/reducers").default;
-        store.replaceReducer(composeReducer(reducer));
-    });
+  module.hot.accept('_/src/reducers', () => {
+    let reducer = require('_/src/reducers').default;
+    store.replaceReducer(composeReducer(reducer));
+  });
 }
